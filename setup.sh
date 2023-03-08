ecc859b52c19e9ae1def69444fc7ac0c86ad0785
lars <lars@dev.speedport.ip>
2023-03-08 03:47:09 +0100

    fix

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ cd $TMP_DIR
 
 # Install packages
 X11_PACKAGES="xorg"
-DESKTOP_PACKAGES="lightdm polybar picom dunst rofi bspwm sxhd zsh feh neofetch imagemagick"
+DESKTOP_PACKAGES="lightdm polybar picom dunst rofi bspwm sxhkd zsh feh neofetch imagemagick"
 UTILITY_PACKAGES="git wget unzip python3"
 
 echo -n "Do you want to install nvidia drivers? (yes/no)"
@@ -17,16 +17,16 @@ fi
 
 # Enable non-free packages for nvidia driver
 if grep -q non-free /etc/apt/sources.list; then
-    sed -i 's/main/main contrib non-free/g' /etc/apt/sources.list
+    sudo sed -i 's/main/main contrib non-free/g' /etc/apt/sources.list
 fi
 
-apt-get update 
-apt-get -y install $X11_PACKAGES $DESKTOP_PACKAGES $UTILITY_PACKAGES
-pip3 install pywal
+sudo apt-get update 
+sudo apt-get -y install $X11_PACKAGES $DESKTOP_PACKAGES $UTILITY_PACKAGES
+sudo pip3 install pywal
 
 #Confiure Xorg and Lightdm
-Xorg -configure
-dpkg-reconfigure lightdm
+sudo Xorg -configure
+sudo dpkg-reconfigure lightdm
 
 # Add BSPWM Entry
 echo "[Desktop Entry]\nName=bspwm\nComment=Binary space partitioning window manager\nExec=bspwm\nType=Application" > /usr/share/xsessions/bspwm.desktop
@@ -39,41 +39,28 @@ ln -s $HOME/.config/dots/.config/picom $HOME/.config/picom
 ln -s $HOME/.config/dots/.config/polybar $HOME/.config/polybar
 ln -s $HOME/.config/dots/.config/rofi $HOME/.config/rofi
 ln -s $HOME/.config/dots/.config/sxhkd $HOME/.config/sxhkd
+ln -s $HOME/.config/dots/.config/.zshrc $HOME/.zshrc
 
 # Setup Fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Meslo.zip -O Meslo.zip
 unzip Meslo.zip -d Meslo
-mv Meslo/*.ttf /usr/share/fonts/truetype
+sudo mv Meslo/*.ttf /usr/share/fonts/truetype
 cd /usr/share/fonts/truetype
-mkfontscale
-mkfontdir
-fc-cache
-xset fp rehash
+sudo mkfontscale
+sudo mkfontdir
+sudo fc-cache
+sudo xset fp rehash
 cd $TMP_DIR
+
+# Disable services slowing the boot process
+sudo systemctl disable NetworkManager-wait-online.service
 
 # Setup Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-# Download useful apps
-## Spotify
-curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
-echo "deb http://repository.spotify.com stable non-free" | tee /etc/apt/sources.list.d/spotify.list
-apt-get update && apt-get -y install spotify-client
-
-## Discord
-wget https://discord.com/api/download\?platform\=linux\&format\=deb-O discord.deb
-dpkg -i discord.deb
-
-## VS Code
-wget https://go.microsoft.com/fwlink/?LinkID=760868 -O vscode.deb
-dpkg -i vscode.deb
-
-# Disable services slowing the boot process
-systemctl disable NetworkManager-wait-online.service
-
 # DONE
-clear
+#clear
 echo Done! Setup Complete 
 neofetch
 echo TODO: Set ZSH_THEME="powerlevel10k/powerlevel10k" in ~/.zshrc
