704cae8b30b60915d0afc2604bd3d0f4ede5affe
lars <lars@dev.speedport.ip>
2023-03-12 16:56:09 +0100

    manjaro

--- setup.sh
+++ setup.sh
@@ -1,29 +1,19 @@
 #!/bin/bash
-TMP_DIR="/tmp/epic-setup"
+TMP_DIR="/tmp/rice-setup"
 
 mkdir $TMP_DIR
 cd $TMP_DIR
 
 # Install packages
-DESKTOP_PACKAGES="polybar dunst rofi bspwm sxhkd zsh feh neofetch imagemagick unclutter"
+DESKTOP_PACKAGES="polybar picom dunst rofi bspwm sxhkd zsh feh neofetch imagemagick unclutter"
 UTILITY_PACKAGES="git wget unzip python3"
-PICOM_DEPS="libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl-dev libegl-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev meson build-essential ninja-build"
-NVIDIA_PACKAGES=""
 
-echo -n "Do you want to install nvidia drivers? (yes/no)"
-read userInput
-if [ "$userInput" = "yes" ]; then
-    NVIDIA_PACKAGES="nvidia-driver"
-fi
-
-# Enable non-free packages for nvidia driver
-if ! grep -q non-free /etc/apt/sources.list; then
-    sudo sed -i 's/main/main contrib non-free/g' /etc/apt/sources.list
-fi
+PM_UPDATE_COMMAND = "pacman -Syu"
+PM_INSTALL_COMMAND = "pacman -S"
 
 # Get new packages
-sudo apt-get update 
-sudo apt-get -y install git
+sudo $PM_UPDATE_COMMAND
+sudo $PM_INSTALL_COMMAND $UTILITY_PACKAGES
 
 # Pull dots git repo
 git clone https://github.com/PlatinShadow/dots.git
@@ -34,22 +24,13 @@ sudo ln -s $HOME/.config/dots/.config/picom $HOME/.config/picom
 sudo ln -s $HOME/.config/dots/.config/polybar $HOME/.config/polybar
 sudo ln -s $HOME/.config/dots/.config/rofi $HOME/.config/rofi
 sudo ln -s $HOME/.config/dots/.config/sxhkd $HOME/.config/sxhkd
-rmdir $HOME/.config/gtk-3.0 $HOME/.config/gtk-4.0
+sudo rmdir $HOME/.config/gtk-3.0 $HOME/.config/gtk-4.0
 sudo ln -s $HOME/.config/dots/.config/gtk-3.0 $HOME/.config/gtk-3.0
 sudo ln -s $HOME/.config/dots/.config/gtk-4.0 $HOME/.config/gtk-4.0
 
 # Download all packages
-sudo apt-get -y install $DESKTOP_PACKAGES $UTILITY_PACKAGES $PICOM_DEPS $NVIDIA_PACKAGES
+sudo $PM_INSTALL_COMMAND $DESKTOP_PACKAGES
 sudo pip3 install pywal
-
-# Compile picom
-git clone https://github.com/yshui/picom.git
-cd picom
-git submodule update --init --recursive
-meson setup --buildtype=release . build
-ninja -C build
-sudo ninja -C build install
-cd $TMP_DIR
 
 # Setup Fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Meslo.zip -O Meslo.zip
@@ -63,7 +44,7 @@ sudo xset fp rehash
 cd $TMP_DIR
 
 # Setup Services
-sudo systemctl disable NetworkManager-wait-online.service
+#sudo systemctl disable NetworkManager-wait-online.service
 
 # Setup Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
