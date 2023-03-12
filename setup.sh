71db14659341b6c920f320d5efc260c8f7d31986
lars <lars@dev.speedport.ip>
2023-03-12 17:32:32 +0100

    rename

--- setup.sh
+++ setup.sh
@@ -5,11 +5,14 @@ mkdir $TMP_DIR
 cd $TMP_DIR
 
 # Install packages
-DESKTOP_PACKAGES="polybar picom dunst rofi bspwm sxhkd zsh feh neofetch imagemagick unclutter"
+DESKTOP_PACKAGES="polybar picom dunst rofi bspwm sxhkd zsh feh neofetch imagemagick unclutter xorg-xsetroot" 
 UTILITY_PACKAGES="git wget unzip python3 python-pip"
 
 PM_UPDATE_COMMAND="pacman -Syu"
 PM_INSTALL_COMMAND="pacman -S"
+
+#install graphics driver
+sudo mhwd -a pci nonfree 0300
 
 # Get new packages
 sudo $PM_UPDATE_COMMAND
@@ -19,14 +22,14 @@ sudo $PM_INSTALL_COMMAND $UTILITY_PACKAGES
 git clone https://github.com/PlatinShadow/dots.git
 mkdir $HOME/.config
 sudo mv dots $HOME/.config/dots
-sudo ln -s $HOME/.config/dots/.config/bspwm $HOME/.config/bspwm
-sudo ln -s $HOME/.config/dots/.config/picom $HOME/.config/picom
-sudo ln -s $HOME/.config/dots/.config/polybar $HOME/.config/polybar
-sudo ln -s $HOME/.config/dots/.config/rofi $HOME/.config/rofi
-sudo ln -s $HOME/.config/dots/.config/sxhkd $HOME/.config/sxhkd
+sudo ln -s $HOME/.config/dots/config/bspwm $HOME/.config/bspwm
+sudo ln -s $HOME/.config/dots/config/picom $HOME/.config/picom
+sudo ln -s $HOME/.config/dots/config/polybar $HOME/.config/polybar
+sudo ln -s $HOME/.config/dots/config/rofi $HOME/.config/rofi
+sudo ln -s $HOME/.config/dots/config/sxhkd $HOME/.config/sxhkd
 sudo rmdir $HOME/.config/gtk-3.0 $HOME/.config/gtk-4.0
-sudo ln -s $HOME/.config/dots/.config/gtk-3.0 $HOME/.config/gtk-3.0
-sudo ln -s $HOME/.config/dots/.config/gtk-4.0 $HOME/.config/gtk-4.0
+sudo ln -s $HOME/.config/dots/config/gtk-3.0 $HOME/.config/gtk-3.0
+sudo ln -s $HOME/.config/dots/config/gtk-4.0 $HOME/.config/gtk-4.0
 
 # Download all packages
 sudo $PM_INSTALL_COMMAND $DESKTOP_PACKAGES
@@ -48,13 +51,13 @@ cd $TMP_DIR
 
 # Setup Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-ecit 
+exit 
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 sudo rm $HOME/.zshrc $HOME/.p10k.zsh
-sudo ln -s $HOME/.config/dots/.config/.zshrc $HOME/.zshrc
-sudo ln -s $HOME/.config/dots/.config/.p10k.zsh $HOME/.p10k.zsh
+sudo ln -s $HOME/.config/dots/config/.zshrc $HOME/.zshrc
+sudo ln -s $HOME/.config/dots/config/.p10k.zsh $HOME/.p10k.zsh
 
 # DONE
 neofetch
